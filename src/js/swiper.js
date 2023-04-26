cfd8a63532c8d2bc31fba15c1f7c4b7a410cd316
groguls <125222488+groguls@users.noreply.github.com>
2023-04-26 09:20:33 +0200

    js slider fixed

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -9,21 +9,56 @@ const swiper = new Swiper('.hero-swiper', {
     nextEl: '.button-next',
     prevEl: '.button-prev',
   },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
-    renderFraction: function (currentClass, totalClass) {
-      return (
-        '<span class="' +
-        currentClass +
-        '"></span>' +
-        '<span class="' +
-        paginationLine +
-        '"></span>' +
-        '<span class="' +
-        totalClass +
-        '"></span>'
-      );
-    },
+pagination: {
+  el: '.swiper-pagination',
+  type: 'fraction',
+  renderFraction: function (currentClass, totalClass) {
+    return (
+      '<span class="' +
+      currentClass +
+      '"></span>' +
+      '<span class="pagination-line"></span>' +
+      '<span class="' +
+      totalClass +
+      '"></span>'
+    );
   },
+  formatFractionCurrent: function (number) {
+    return number < 10 ? '0' + number : number;
+  },
+  formatFractionTotal: function (number) {
+    return number < 10 ? '0' + number : number;
+  },
+},
 });
+
+
+// const swiper = new Swiper('.hero-swiper', {
+//   effect: 'fade',
+
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   loop: true,
+//   keyboard: { enabled: true },
+//   navigation: {
+//     nextEl: '.button-next',
+//     prevEl: '.button-prev',
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     type: 'fraction',
+//     renderFraction: function (currentClass, totalClass) {
+//       return (
+//         '<span class="' +
+//         currentClass +
+//         '"></span>' +
+//         '<span class="' +
+//         paginationLine +
+//         '"></span>' +
+//         '<span class="' +
+//         totalClass +
+//         '"></span>'
+//       );
+//     },
+//   },
+// });
