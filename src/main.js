c739940864b00fdab6652ffc9b4bb711026a67e2
Mykola Ts <tsybulskiyk@gmail.com>
2025-06-22 15:44:01 +0300

    added functionality show/hide toggle for product description and details in Sale section

--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import * as mobileMenu from './js/mobile-menu';
 import * as swiper from './js/swiper';
 import * as modal from './js/modal';
 import * as catalog from './js/catalog';
+import * as sale from './js/sale';
